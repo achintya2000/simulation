536cbc50033fe7d719ffa15ebf5825ee282ac033
Sarah Gregorich <seg47@duke.edu>
2020-01-30 20:09:38 -0500

    Updated abstract Grid class and implementations of array grid methods

--- src/cellsociety/Model/ArrayGrid.java
+++ src/cellsociety/Model/ArrayGrid.java
@@ -10,69 +10,74 @@ public class ArrayGrid extends Grid {
 
     public ArrayGrid(int size) { // assume its a square
         mySize = size;
-        initialize();
-    }
-
-    @Override
-    public void initialize() {
         myArray = new int[mySize][mySize];
         for (int i = 0; i < mySize; i++) {
             for (int j = 0; j < mySize; j++) {
-                myArray[i][j] = i;
+                myArray[i][j] = -1;
             }
         }
     }
 
     @Override
-    public void update() {
-        //updates myArray by make a copy of the array as a reference
-        // logic for update based on neighbors goes here
-        // updates for game of life only with refactor to make more general
+    public int getSize() {
+        return mySize;
+    }
 
-        myReferenceArray = new int[myArray.length][myArray[0].length];
-        System.arraycopy(myArray, 0, myReferenceArray, 0, myArray.length);    //makes new copy of myArray so the method has reference
-        for (int cellRow = 0; cellRow < myArray.length; cellRow++) {                        // loops through each row of myArray
-            for (int cellCol = 0; cellCol < myArray[0].length; cellCol++) {                 // loops through each col of myArray
-                int aliveNeighbors = checkNeighbors(cellRow, cellCol, myReferenceArray);
-                //insert method that performs certain logic based on state of neighbors and state of current cell
-                // lines 35 - 44 are just for game of life
-                if(getState(cellRow,cellCol) == 0 && aliveNeighbors == 3){           // a dead cell with exactly 3 neighbors comes back to life
-                    myArray[cellRow][cellCol] = 1;
-                }
-                else if(getState(cellRow,cellCol) == 1 && aliveNeighbors < 2){       // an alive cell dies if less than 2 alive neighbors
-                    myArray[cellRow][cellCol] = 0;
-                }
-                else if(getState(cellRow,cellCol) == 1 && aliveNeighbors > 3){       // an alive cell with more then 3 neighbors dies
-                    myArray[cellRow][cellCol] = 0;
+    @Override
+    public void initializeCell(int row, int col, int state) {
+        myArray[row][col] = state;
+    }
+
+    @Override
+    public void initializeDefaultCell(int state) {
+        for (int i = 0; i < mySize; i++) {
+            for (int j = 0; j < mySize; j++) {
+                if (myArray[i][j] == -1) {
+                    myArray[i][j] = state;
                 }
             }
         }
     }
 
-   private int checkNeighbors(int cellRow, int cellCol, int[][] referenceArray){
-        int[] rDelta = {0,0,1,-1,1,1,-1,-1};                    //parallel arrays rDelta and cDelta check each possible neighbor of a cell
+    @Override
+    public void updateCell(int row, int col, int newState) {
+        myArray[row][col] = newState;
+    }
+
+    @Override
+    public int[] checkNeighbors(int row, int col){
+        if (row==0 && col==0) {
+            myReferenceArray = myArray;
+        }
+        int numNeighbors = 0;
+        int[] neighbors = new int[8];
+        int[] rDelta = {0,0,1,-1,1,1,-1,-1};
         int[] cDelta = {1,-1,0,0,1,-1,1,-1};
-                int aliveNeighbors = 0;                         // this is for just game of life will refactor to make more general
-                for(int i = 0; i < rDelta.length; i ++){        // loops through rDelta and cDelta - checking each neighbor
-                    int neighborRow = cellRow + rDelta[i];
-                    int neighborCol = cellCol + cDelta[i];
-                    if(inBounds(neighborRow, neighborCol)){
-                        // insert method to perform some logic to check state of neighbors simulation specific
-                        if(getState(neighborRow,neighborCol) == 1){ //game of life logic
-                            aliveNeighbors++;
-                        }
-                    }
-                }
-        return aliveNeighbors;
+        for(int i = 0; i < rDelta.length; i ++) {
+           int neighborRow = row + rDelta[i];
+           int neighborCol = col + cDelta[i];
+           if (inBounds(neighborRow, neighborCol)) {
+               neighbors[numNeighbors] = getReferenceState(neighborRow,neighborCol);
+               numNeighbors = numNeighbors + 1;
+           }
+        }
+        for (int i = 0; i < (8-numNeighbors); i ++) {
+            neighbors[numNeighbors] = -1;
+            numNeighbors = numNeighbors + 1;
+        }
+       return neighbors;
     }
-
 
     @Override
-    public int getState(int pos_x, int pos_y) {
-        return myReferenceArray[pos_x][pos_y];
+    public int getCurrentState(int row, int col) {
+        return myArray[row][col];
     }
 
-    private boolean inBounds(int r, int c){ //
+    private int getReferenceState(int row, int col) {
+        return myReferenceArray[row][col];
+    }
+
+    private boolean inBounds(int r, int c){
         return (r < myArray.length && r >= 0 && c < myArray[0].length && c >= 0);
     }
 
