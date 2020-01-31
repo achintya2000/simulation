83bd5987f9cd1bb44b4a599ce342230963c04b48
Doherty Guirand <dg211@duke.edu>
2020-01-30 21:43:06 -0500

    updated percolation

--- src/cellsociety/Controller/Percolation.java
+++ src/cellsociety/Controller/Percolation.java
@@ -1,25 +1,51 @@
 package cellsociety.Controller;
 
+import cellsociety.Model.ArrayGrid;
 import cellsociety.Model.Grid;
 
-/*public class Percolation extends Simulation {
-    public Percolation(String filepath) {
-        super(filepath);
+import java.io.File;
+import java.util.Map;
+
+// 0 - can flow no water
+// 1 - can flow has water
+// 2 - cannot flow
+
+public class Percolation extends Simulation {
+
+    @Override
+    void loadSimulationContents(String filepath) {
+        XMLParser Percolation = new XMLParser("config");
+        Map<String, String> configuration = percolation.getInfo(new File(filepath));
+        SIMULATION_NAME = configuration.get("simulation");
+        GRID_WIDTH = Integer.parseInt(configuration.get("width"));
+        GRID_HEIGHT = Integer.parseInt(configuration.get("height"));
+        System.out.println(configuration);
+        simulationGrid = new ArrayGrid(GRID_WIDTH);
     }
 
     @Override
     void updateGrid() {
-        for(int r = 0; r < Grid.getSize(); r ++){
-            for(int c = 0; c < Grid.getSize(); c ++){
+        for(int r = 0; r < simulationGrid.getSize(); r ++){
+            for(int c = 0; c < simulationGrid.getSize(); c ++){
                 if(canFlow(r,c) && closeToWater(r,c)){
-                    Grid.updateCell(r,c,1);
+                    simulationGrid.updateCell(r,c,1);
                 }
             }
         }
     }
 
-public boolean closeToWater(int r, int c){
-        int[] statusOfNeighbors = Grid.checkNeighbors(r,c);
+    @Override
+    Grid getGrid() {
+        return simulationGrid;
+    }
+
+    @Override
+    int getSimulationCols() {
+        return GRID_WIDTH;
+    }
+
+    public boolean closeToWater(int r, int c){
+        int[] statusOfNeighbors = simulationGrid.checkNeighbors(r,c);
         int i = 0;
         while (statusOfNeighbors[i] != -1){
             if(statusOfNeighbors[i] == 1){// has water might need to change based on numbering
@@ -30,6 +56,6 @@ public boolean closeToWater(int r, int c){
     }
 
     public boolean canFlow(int r, int c){
-        return (Grid.getCurrentState() != 2); //cell does not flow water
+        return (simulationGrid.getCurrentState(r,c) != 2); //cell does not flow water
     }
 }
