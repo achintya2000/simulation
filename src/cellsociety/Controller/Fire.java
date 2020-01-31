0d90c7592b01f4677e4b2ef50e9a137884e55a2c
Sarah Gregorich <seg47@duke.edu>
2020-01-31 09:35:42 -0500

    Refactors loadSimulation method, added Fire

--- src/cellsociety/Controller/Fire.java
+++ src/cellsociety/Controller/Fire.java
@@ -7,13 +7,13 @@ import java.util.*;
 
 import javafx.scene.paint.Color;
 
-public class GameOfLife extends Simulation {
+public class Fire extends Simulation {
 
 
     public void loadSimulationContents(String filepath) {
 
         // Change below to list of cell types to change for each sim
-        List<String> cellTypes = List.of("live");
+        List<String> cellTypes = List.of("tree","burning");
         // See above
 
         List<String> xmlvals = new ArrayList<String>();
@@ -52,21 +52,8 @@ public class GameOfLife extends Simulation {
 
     @Override
     public void updateGrid() {
-        for(int r = 0; r < simulationGrid.getSize(); r ++){
-            for(int c = 0; c < simulationGrid.getSize(); c ++){
-                int aliveNeighbors = aliveNeighbors(r,c);
-                if(simulationGrid.getReferenceState(r,c) == 1 && aliveNeighbors < 2){       // an alive cell dies if less than 2 alive neighbors
-                    simulationGrid.updateCell(r,c,0);
-                }
-                else if(simulationGrid.getReferenceState(r,c) == 1 && aliveNeighbors >= 2 && aliveNeighbors <= 3){
-                    simulationGrid.updateCell(r,c,1);
-                }
-                else if(simulationGrid.getReferenceState(r,c) == 1 && aliveNeighbors > 3){       // an alive cell with more then 3 neighbors dies
-                    simulationGrid.updateCell(r,c,0);
-                }
-                else if(simulationGrid.getReferenceState(r,c) == 0 && aliveNeighbors == 3){           // a dead cell with exactly 3 neighbors comes back to life
-                    simulationGrid.updateCell(r,c,1);
-                }
+        for(int r = 0; r < simulationGrid.getSize(); r ++) {
+            for (int c = 0; c < simulationGrid.getSize(); c++) {
             }
         }
         System.out.println(Arrays.deepToString(simulationGrid.getGrid()));
@@ -92,19 +79,6 @@ public class GameOfLife extends Simulation {
     @Override
     public Map<Integer, Color> getCellColorMap() {
         return cellColorMap;
-    }
-
-    private int aliveNeighbors(int r, int c){
-        int alive = 0;
-        int[] statusOfNeighbors = simulationGrid.checkNeighbors(r,c);
-        int i = 0;
-        while (i < statusOfNeighbors.length && statusOfNeighbors[i] != -1 ){
-            if(statusOfNeighbors[i] == 1){
-                alive++;
-            }
-            i++;
-        }
-        return alive;
     }
 
 }
