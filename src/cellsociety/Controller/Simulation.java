125f51f911ef7a620c32dd68de7a1881621b43a9
Sarah Gregorich <seg47@duke.edu>
2020-02-09 17:48:51 -0500

    Refactored longest method in SIm

--- src/cellsociety/Controller/Simulation.java
+++ src/cellsociety/Controller/Simulation.java
@@ -42,22 +42,8 @@ public abstract class Simulation {
   }
 
   public void loadSimulationContents(File simFile, String simName, boolean random) {
-    List<String> numTypesRequest = new ArrayList<String>();
-    numTypesRequest.addAll(List.of(simName+"numtypes"));
-    XMLParser metaParser = new XMLParser("config");
-    Map<String, String> numTypesFromFile = metaParser.getInfo(infoFile, numTypesRequest);
-    int numtypes = Integer.parseInt(numTypesFromFile.get(simName+"numtypes"));
-
-    List<String> cellTypes = new ArrayList<String>();
-    for (int i = 0; i < numtypes-1; i ++) {
-      cellTypes.add("celltype"+i);
-    }
-
-    List<String> xmlvals = new ArrayList<String>();
-    xmlvals.addAll(List.of("title", "author", "simulation", "width", "height","default"));
-    for (String celltype : cellTypes) {
-      xmlvals.addAll(List.of("num"+celltype, "state"+celltype,celltype));
-    }
+    List<String> cellTypes = getCellTypes(simName);
+    List<String> xmlvals = getXMLTags(cellTypes);
     XMLParser simParser = new XMLParser("config");
     Map<String, String> configuration = simParser.getInfo(simFile, xmlvals);
 
@@ -67,34 +53,65 @@ public abstract class Simulation {
 
     simulationGrid = new ArrayGrid(GRID_WIDTH);
 
-    initializeGrid(cellTypes, configuration, random, numtypes);
+    initializeGrid(cellTypes, configuration, random, cellTypes.size()+1);
     init();
+  }
+
+  private List<String> getCellTypes(String simName) {
+    List<String> numTypesRequest = new ArrayList<String>();
+    numTypesRequest.addAll(List.of(simName+"numtypes"));
+    XMLParser metaParser = new XMLParser("config");
+    Map<String, String> numTypesFromFile = metaParser.getInfo(infoFile, numTypesRequest);
+    int numtypes = Integer.parseInt(numTypesFromFile.get(simName+"numtypes"));
+    List<String> cellTypes = new ArrayList<String>();
+    for (int i = 0; i < numtypes-1; i ++) {
+      cellTypes.add("celltype"+i);
+    }
+    return cellTypes;
+  }
+
+  private List<String> getXMLTags(List<String> cellTypes) {
+    List<String> xmlvals = new ArrayList<String>();
+    xmlvals.addAll(List.of("title", "author", "simulation", "width", "height","default"));
+    for (String celltype : cellTypes) {
+      xmlvals.addAll(List.of("num"+celltype, "state"+celltype,celltype));
+    }
+    return xmlvals;
+
   }
 
   protected void initializeGrid(List<String> cellTypes, Map<String, String> configuration, boolean random, int range) {
     if (random){
-      for (int r = 0; r < simulationGrid.getSize(); r++) {
-        for (int c = 0; c < simulationGrid.getSize(); c++) {
-          int randomNum = ThreadLocalRandom.current().nextInt(0, range);
-          simulationGrid.updateCell(r, c, randomNum);
-        }
-      }
+      populateRandomGrid(range);
     } else {
-      String[] point = new String[2];
-      for (String celltype : cellTypes) {
-        String cellLocations = configuration.get(celltype);
-        int k = 0;
-        while (cellLocations.lastIndexOf("]") != cellLocations.indexOf("]")) {
-          point = (cellLocations
-              .substring(cellLocations.indexOf("[") + 1, cellLocations.indexOf("]"))).split(",");
-          simulationGrid.updateCell(Integer.parseInt(point[0]), Integer.parseInt(point[1]),
-              Integer.parseInt(configuration.get("state" + celltype)));
-          cellLocations = cellLocations
-              .substring(cellLocations.indexOf("]") + 1, cellLocations.lastIndexOf("]") + 1);
-          k = k + 1;
-        }
+      populateFileGrid(cellTypes, configuration);
+    }
+  }
+
+  private void populateFileGrid(List<String> cellTypes, Map<String, String> configuration) {
+    String[] point = new String[2];
+    for (String celltype : cellTypes) {
+      String cellLocations = configuration.get(celltype);
+      int k = 0;
+      while (cellLocations.lastIndexOf("]") != cellLocations.indexOf("]")) {
+        point = (cellLocations
+            .substring(cellLocations.indexOf("[") + 1, cellLocations.indexOf("]"))).split(",");
+        simulationGrid.updateCell(Integer.parseInt(point[0]), Integer.parseInt(point[1]),
+            Integer.parseInt(configuration.get("state" + celltype)));
+        cellLocations = cellLocations
+            .substring(cellLocations.indexOf("]") + 1, cellLocations.lastIndexOf("]") + 1);
+        k = k + 1;
       }
-      simulationGrid.initializeDefaultCell(Integer.parseInt(configuration.get("default")));
+    }
+    simulationGrid.initializeDefaultCell(Integer.parseInt(configuration.get("default")));
+  }
+
+  private void populateRandomGrid(int range) {
+    for (int r = 0; r < simulationGrid.getSize(); r++) {
+      for (int c = 0; c < simulationGrid.getSize(); c++) {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, range);
+        simulationGrid.updateCell(r, c, randomNum);
+      }
     }
   }
 
--- src/cellsociety/Controller/Wator.java
+++ src/cellsociety/Controller/Wator.java
@@ -89,19 +89,17 @@ public class Wator extends Simulation{
     boolean fishEaten = false;
     // Below determines if there are any fish around the shark, if they are, they are eaten
     for (Map.Entry<String,Integer> entry : statusOfNeighbors.entrySet()) {
-      if (entry.getValue() == fish) { // take first neighbor that is fish
-        if (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1])) { // this needs to get fixed asap
-          sharkEnergy[r+ simulationGrid.getOffset(entry.getKey())[0]][c+ simulationGrid.getOffset(entry.getKey())[1]] = sharkEnergy[r][c]+1;
-          sharkEnergy[r][c] = 0;
-          simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], shark);
-          simulationGrid.updateCell(r, c, empty);
-          fishEaten=true;
-          if (chronon % 5 == 0) {
-            sharkEnergy[r][c] = shark_lives;
-            simulationGrid.updateCell(r,c, shark);
-          }
-          break;
+      if (entry.getValue() == fish && (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1]))) {
+        sharkEnergy[r+ simulationGrid.getOffset(entry.getKey())[0]][c+ simulationGrid.getOffset(entry.getKey())[1]] = sharkEnergy[r][c]+1;
+        sharkEnergy[r][c] = 0;
+        simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], shark);
+        simulationGrid.updateCell(r, c, empty);
+        fishEaten=true;
+        if (chronon % 5 == 0) {
+          sharkEnergy[r][c] = shark_lives;
+          simulationGrid.updateCell(r,c, shark);
         }
+        break;
       }
     }
     return fishEaten;
@@ -110,34 +108,29 @@ public class Wator extends Simulation{
   private void moveShark(Map<String,Integer> statusOfNeighbors, int r, int c) {
     // If the shark did not move to eat the fish, and a nearby location is empty, move
     for (Map.Entry<String,Integer> entry : statusOfNeighbors.entrySet()) {
-      if (entry.getValue() == empty) {
-        if (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1])) {
-          sharkEnergy[r+simulationGrid.getOffset(entry.getKey())[0]][c+simulationGrid.getOffset(entry.getKey())[1]]=sharkEnergy[r][c];
-          sharkEnergy[r][c]=0;
-          simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], shark);
-          simulationGrid.updateCell(r, c, empty);
-          if (chronon % 5 == 0) {
-            sharkEnergy[r][c] = shark_lives;
-            simulationGrid.updateCell(r,c, shark);
-          }
-          break;
+      if (entry.getValue() == empty && (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1]))) {
+        sharkEnergy[r+simulationGrid.getOffset(entry.getKey())[0]][c+simulationGrid.getOffset(entry.getKey())[1]]=sharkEnergy[r][c];
+        sharkEnergy[r][c]=0;
+        simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], shark);
+        simulationGrid.updateCell(r, c, empty);
+        if (chronon % 5 == 0) {
+          sharkEnergy[r][c] = shark_lives;
+          simulationGrid.updateCell(r,c, shark);
         }
+        break;
       }
     }
   }
 
   private void fishGoesTo(int r, int c) {
     Map<String,Integer> statusOfNeighbors = simulationGrid.checkNeighbors(r, c,  false);
-
     for (Map.Entry<String,Integer> entry : statusOfNeighbors.entrySet()) {
-      if (entry.getValue() == empty) {
-        if (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1])) {
-          simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], fish);
-          if (chronon % 5 != 0) { // Put fish in new spot
-            simulationGrid.updateCell(r, c, empty);
-          }
-          break;
+      if (entry.getValue() == empty && (simulationGrid.inBounds(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1]))) {
+        simulationGrid.updateCell(r + simulationGrid.getOffset(entry.getKey())[0], c + simulationGrid.getOffset(entry.getKey())[1], fish);
+        if (chronon % 5 != 0) { // Put fish in new spot
+          simulationGrid.updateCell(r, c, empty);
         }
+        break;
       }
     }
 
