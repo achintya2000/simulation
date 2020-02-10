1b962051181ac5949e54291d6203b2cae02f2bd6
Sarah Gregorich <seg47@duke.edu>
2020-02-09 22:42:16 -0500

    Made grid an interface

--- src/cellsociety/Model/Grid.java
+++ src/cellsociety/Model/Grid.java
@@ -3,16 +3,16 @@ package cellsociety.Model;
 import java.util.List;
 import java.util.Map;
 
-public abstract class Grid {
-    public abstract int getSize();
-    public abstract void initializeDefaultCell(int state);
-    public abstract void updateCell(int row, int col, int newState);
-    public abstract void setNeighbors(List<String> requestedNeighbors, int shape, int edge);
-    public abstract Map<String, Integer> checkNeighbors(int row, int col, boolean atomicUpdate);
-    public abstract Integer[] getOffset(String neighbor);
-    public abstract int getCurrentState(int row, int col);
-    public abstract int[][] getGrid();
-    public abstract boolean isNeighborhoodSet();
-    public abstract int getReferenceState(int row, int col);
-    public abstract boolean inBounds(int r, int c);
+public interface Grid {
+    int getSize();
+    void initializeDefaultCell(int state);
+    void updateCell(int row, int col, int newState);
+    void setNeighbors(List<String> requestedNeighbors, int shape, int edge);
+    Map<String, Integer> checkNeighbors(int row, int col, boolean atomicUpdate);
+    Integer[] getOffset(String neighbor);
+    int getCurrentState(int row, int col);
+    int[][] getGrid();
+    boolean isNeighborhoodSet();
+    int getReferenceState(int row, int col);
+    boolean inBounds(int r, int c);
 }
